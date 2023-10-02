5adb2f2c6a26ced1bcdaa41f56e8544423397b38
nimisha <nimishasrivastava14@gmail.com>
2023-10-01 21:45:11 -0400

    leetcode

---
+++ FirstBadVersion.java
@@ -0,0 +1,17 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        int firstBadVersion = -1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid)) {
+                firstBadVersion = mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return firstBadVersion;
+    }
+}
